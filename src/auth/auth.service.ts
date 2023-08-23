5599764201bc8bb132e2b6135415907a57eafe56
Antonio Fulgencio <antoniofulg@gmail.com>
2023-08-23 00:59:39 -0300

    feat: add login mutation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { LoginUserInput } from './dto/login-user.input';
 
 @Injectable()
 export class AuthService {
@@ -7,5 +8,24 @@ export class AuthService {
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(username);
+
+    if (user && user.password === password) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user;
+      return result;
+    }
+
+    return null;
+  }
+
+  async login(loginUserInput: LoginUserInput) {
+    const user = await this.usersService.findOne(loginUserInput.username);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...result } = user;
+
+    return {
+      access_token: 'jwt',
+      user: result,
+    };
   }
 }
